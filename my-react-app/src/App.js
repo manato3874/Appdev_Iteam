44cd1dc4f9b72987ca7be8e8a55a18f2946b9839
m <managgmanaggmana@icluod.com>
2023-12-07 14:28:00 +0900

    react開発環境

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,21 +1,32 @@
+// 例: App.js の修正
 import React from 'react';
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Page2 from './components/Page2';
 import './App.css';
 
-function MyButton() {
-
+function App() {
   return (
-    <button id="startbtn">
-      I'm a button
-    </button>
+    <Router>
+      <div className="App">
+        <h1>トイレの神様</h1>
+        <button id="startbtn">
+        <Link to="/Home" className="link">やばい漏れそう</Link>
+        </button>
+        <br />
+        <br />
+        <button id="chatbtn">
+        <Link to="/page2" className="link">チャット</Link>
+        </button>
+        <br />
 
+        <Routes> {/* ここで Routes を使用する */}
+          <Route path="/Home" element={<Home />} />
+          <Route path="/page2" element={<Page2 />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-export default function Myindex(){
-  return(
-    <div>
-      <h1>便所の神様</h1>
-      <MyButton/>
-    </div>
-  )
-}
+export default App;
