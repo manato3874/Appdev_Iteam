a32db02a56a315e0c7a54f81bf774258205143f7
shogo <85502213+Shogo-Furuyama@users.noreply.github.com>
2024-01-26 14:43:24 +0900

    一応形にしました

--- nextjs/src/app/wc.tsx
+++ nextjs/src/app/wc.tsx
@@ -1,5 +1,9 @@
 // wc.tsx
 import Head from 'next/head';
+import Map_iframe from '../../data/iframe.json';
+import Opendata from '../../data/opendata.json';
+import Maps from '../../data/test.json';
+
 
 const WCPage = () => {
   return (
@@ -11,6 +15,18 @@ const WCPage = () => {
       <h1>トイレへようこそ！</h1>
       <p>ここはトイレページです。何かトイレに関する内容をここに書くことができます。</p>
 
+      <div>
+        <ul>
+          {Maps.map((data, index) => (
+            <li key={index}>
+              <p>{data.施設名}</p>
+              <p>{data.都道府県} {data.市区町村} {data.番地}</p>
+              {data.Map_iframe}
+            </li>
+          ))}
+        </ul>
+      </div>
+      
       {/* その他のページ内容 */}
     </div>
   );
