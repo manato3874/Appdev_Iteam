6b3092bdfa89cf193df41e5a337d204bf59af6e9
m <managgmanaggmana@icluod.com>
2024-01-27 23:29:50 +0900

    開発

---
+++ next-app/src/app/page.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import styles from '../../styles/Home.module.css';
+import Map_iframe from '../../data/iframe.json';
+import Opendata from '../../data/opendata.json';
+import Maps from '../../data/toilet.json';
+
+const Page = () => {
+
+  return (
+    <div>
+      <Head>
+        <title>トイレの神様</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <p>トイレの神様</p>
+
+      <Link href="/index" legacyBehavior >
+        <a className={styles.circleButton}>やばいもれそう！！</a>
+      </Link>
+
+      <div>
+        <ul>
+          {Maps.map((data, index) => (
+            <li key={index}>
+              <p>{data.施設名}</p>
+              <p>{data.都道府県} {data.市区町村} {data.番地}</p>
+              <iframe src={data.Map_iframe} width="600" height="450" style={{ marginRight: '10px' }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+    </div>
+  );
+}
+
+export default Page;
---
+++ next-app/src/app/wc.tsx
@@ -0,0 +1,35 @@
+// wc.tsx
+import Head from 'next/head';
+import Map_iframe from '../../data/iframe.json';
+import Opendata from '../../data/opendata.json';
+import Maps from '../../data/test.json';
+
+
+const WCPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>WC Page</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <h1>トイレへようこそ！</h1>
+      <p>ここはトイレページです。何かトイレに関する内容をここに書くことができます。</p>
+
+      <div>
+        <ul>
+          {Maps.map((data, index) => (
+            <li key={index}>
+              <p>{data.施設名}</p>
+              <p>{data.都道府県} {data.市区町村} {data.番地}</p>
+              {data.Map_iframe}
+            </li>
+          ))}
+        </ul>
+      </div>
+      
+      {/* その他のページ内容 */}
+    </div>
+  );
+};
+
+export default WCPage;
