8f948b0fc4dbbcc469fe8cbd047548500ba49b7e
鈴木真斗 <132965270+manato3874@users.noreply.github.com>
2024-01-30 15:07:27 +0900

    Update page.tsx

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -26,7 +26,7 @@ const Page = () => {
             <li key={index}>
               <p>{data.施設名}</p>
               <p>{data.都道府県} {data.市区町村} {data.番地}</p>
-              <iframe src={data.Map_iframe} width="600" height="450" style={{ marginRight: "10px" }} allowFullScreen="true" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <iframe src={data.Map_iframe} width="600" height="450" style={{ marginRight: "10px" }} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
 
 
             </li>
