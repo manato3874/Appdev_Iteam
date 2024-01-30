1801babfc0132d4d669c95a130ccf1f80b32123d
鈴木真斗 <132965270+manato3874@users.noreply.github.com>
2024-01-30 14:54:15 +0900

    Update next.config.js

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    // app: true を app true に変更する
-    true: {},
-  },
 };
 
 module.exports = nextConfig;
